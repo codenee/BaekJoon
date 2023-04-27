5d0c4e6ac5e5ae3671cb689d449545680176b941
codenee <119647209+codenee@users.noreply.github.com>
2023-04-27 12:47:17 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+
+     String str = "";
+     Scanner scanner = new Scanner(System.in);
+
+     str = scanner.next();
+
+     scanner.close();
+
+     System.out.print(str+"??!");
+
+    }
+}
