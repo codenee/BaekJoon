66d5c986b148a18eadce614e15c4ddb8707d8cf8
codenee <119647209+codenee@users.noreply.github.com>
2023-05-14 00:48:53 +0900

    [unrated] Title: 부대복귀, Time: 68.48 ms, Memory: 27.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+    vector<int> answer(sources.size(), 0);
+    
+    vector<vector<int>> graph(n+1);
+    for(const auto& road : roads){
+        graph[road[0]].push_back(road[1]);
+        graph[road[1]].push_back(road[0]);
+    }
+    
+    vector<int> dest(n+1, -1);
+    queue<pair<int, int>> Q;
+    Q.push({destination, 0});
+    dest[destination] = 0;
+    
+    while(!Q.empty()){
+        int curPos = Q.front().first;
+        int curCnt = Q.front().second;
+        Q.pop();
+        for(int i = 0; i < graph[curPos].size(); i++){
+            int nextPos = graph[curPos][i];
+            if(dest[nextPos] == -1 || dest[nextPos] > curCnt + 1){
+                Q.push({nextPos, curCnt+1});
+                dest[nextPos] = curCnt + 1;
+            }
+        }
+    }
+    
+    for(int i = 0; i < sources.size(); i++){
+        answer[i] = dest[sources[i]];
+    }
+    return answer;
+}
