4892f37d101f8496ad008ad43ea9c961c7a4b32c
codenee <119647209+codenee@users.noreply.github.com>
2023-05-21 11:39:24 +0900

    [Gold III] Title: 내리막 길, Time: 40 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -5,12 +5,13 @@
 using namespace std;
 
 int n, m;
-int dist[6] = {1, 0, -1, 0, 1};
-
 vector<vector<int>> board;
 vector<vector<int>> cnt;
 
+int dist[6] = {1, 0, -1, 0, 1};
+
 int dfs(int row, int col){
+
     if(cnt[row][col] >= 0){
         return cnt[row][col];
     }
@@ -18,7 +19,6 @@ int dfs(int row, int col){
     for(int i = 0; i < 4; i++){
         int nr = row + dist[i];
         int nc = col + dist[i+1];
-
         if(nr < 0 || nc < 0 || nr >= n || nc >= m){
             continue;
         }
@@ -26,32 +26,29 @@ int dfs(int row, int col){
         if(board[row][col] > board[nr][nc]){
             cnt[row][col] += dfs(nr, nc);
         }
-
     }
+    
     return cnt[row][col];
 }
 
 int main(){
 
-	//freopen("E:/vscode/inflearn/ex2/input.txt", "r", stdin);
+   scanf("%d %d", &n, &m);
+    board.resize(n, vector<int>(m, 0));
+    cnt.resize(n, vector<int>(m, 0));
 
-    scanf("%d %d", &n, &m);
-
-    board.resize(n, vector<int>(m,0));
-    cnt.resize(n, vector<int>(m,0));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            scanf("%d", &board[i][j]);
-            cnt[i][j] = -1;
-        }
+   for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+        scanf("%d", &board[i][j]);
+        cnt[i][j] = -1;
     }
+   }
 
     cnt[n-1][m-1] = 1;
 
     dfs(0, 0);
 
     printf("%d\n", cnt[0][0]);
-
 
     return 0;
 }
