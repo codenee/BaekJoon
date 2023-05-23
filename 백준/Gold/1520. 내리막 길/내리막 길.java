4d5f00549f54f7eeaa077ced7c437be74ced8363
codenee <119647209+codenee@users.noreply.github.com>
2023-05-23 09:36:26 +0900

    [Gold III] Title: 내리막 길, Time: 388 ms, Memory: 36104 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -5,34 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    private static final int[] dist = {1, 0, -1, 0, 1};
+
     private static int N, M;
     private static int[][] board;
     private static int[][] cnt;
 
-    private static final int[] dist = {1, 0, -1, 0, 1};
-
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       N = Integer.parseInt(st.nextToken());
+       M = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+       board = new int[N][M];
+       cnt = new int[N][M];
 
-        board = new int[N][M];
-        cnt = new int[N][M];
-
-        for(int i = 0; i < N; i++){
+       for (int i = 0; i < N; i++) {
           st = new StringTokenizer(br.readLine());
-          for(int j = 0; j < M; j++){
+          for (int j = 0; j < M; j++) {
             board[i][j] = Integer.parseInt(st.nextToken());
             cnt[i][j] = -1;
           }
-        }
-        
-        cnt[N-1][M-1] = 1;
-        dfs(0, 0);
+       }
 
-        System.out.println(cnt[0][0]);
+       cnt[N-1][M-1] = 1;
+       dfs(0, 0);
+
+       System.out.println(cnt[0][0]);
     }
 
     private static int dfs(int row, int col) {
@@ -41,18 +40,19 @@ public class Main {
       }
 
       cnt[row][col] = 0;
+
       for (int i = 0; i < 4; i++) {
         int nr = row + dist[i];
         int nc = col + dist[i+1];
-
         if(nr < 0 || nc < 0 || nr >= N || nc >= M){
           continue;
         }
-
+        
         if(board[row][col] > board[nr][nc]){
           cnt[row][col] += dfs(nr, nc);
         }
       }
       return cnt[row][col];
     }
+
 }
