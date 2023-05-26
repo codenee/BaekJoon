223786ca0d6194855d4302d6416c20f260d2d0a3
codenee <119647209+codenee@users.noreply.github.com>
2023-05-26 09:21:29 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,14 +1,12 @@
 class Solution {
-   
+    
     private static boolean[] visit;
     private static int[][] computer;
     
     public int solution(int n, int[][] computers) {
         computer = computers;
         visit = new boolean[n];
-        
         int answer = 0;
-        
         for(int i = 0; i < n; i++){
             if(!visit[i]){
                 answer++;
@@ -18,17 +16,15 @@ class Solution {
         
         return answer;
     }
-    
     private static void dfs(int now){
         if(visit[now]){
             return;
         }
         visit[now] = true;
-        for(int i = 0; i < computer.length; i++){
+        for(int i = 0; i < computer[now].length; i++){
             if(computer[now][i] == 1){
                 dfs(i);
             }
         }
     }
-    
 }
