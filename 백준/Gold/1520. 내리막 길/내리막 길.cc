fc559870d5d890431ea06444e05209d515d3aad7
codenee <119647209+codenee@users.noreply.github.com>
2023-05-26 11:00:44 +0900

    [Gold III] Title: 내리막 길, Time: 40 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,54 +1,51 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
 
 using namespace std;
 
+vector<vector<int>> board, cnt;
 int n, m;
-vector<vector<int>> board;
-vector<vector<int>> cnt;
 
-int dist[6] = {1, 0, -1, 0, 1};
+vector<int> dist{ 1, 0, -1, 0, 1 };
 
-int dfs(int row, int col){
+int dfs(int row, int col) {
+	if (cnt[row][col] >= 0) {
+		return cnt[row][col];
+	}
+	cnt[row][col] = 0;
+	
+	for (int i = 0; i < 4; i++) {
+		int nr = row + dist[i];
+		int nc = col + dist[i + 1];
 
-    if(cnt[row][col] >= 0){
-        return cnt[row][col];
-    }
-    cnt[row][col] = 0;
-    for(int i = 0; i < 4; i++){
-        int nr = row + dist[i];
-        int nc = col + dist[i+1];
-        if(nr < 0 || nc < 0 || nr >= n || nc >= m){
-            continue;
-        }
+		if (nr < 0 || nc < 0 || nr >= n || nc >= m) {
+			continue;
+		}
 
-        if(board[row][col] > board[nr][nc]){
-            cnt[row][col] += dfs(nr, nc);
-        }
-    }
-    
-    return cnt[row][col];
+		if (board[row][col] > board[nr][nc]) {
+			cnt[row][col] += dfs(nr, nc);
+		}
+	}
+	return cnt[row][col];
 }
 
-int main(){
+int main() {
 
-   scanf("%d %d", &n, &m);
-    board.resize(n, vector<int>(m, 0));
-    cnt.resize(n, vector<int>(m, 0));
+	scanf("%d %d", &n, &m);
 
-   for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-        scanf("%d", &board[i][j]);
-        cnt[i][j] = -1;
-    }
-   }
+	board.resize(n, vector<int>(m, 0));
+	cnt.resize(n, vector<int>(m, -1));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &board[i][j]);
+		}
+	}
 
-    cnt[n-1][m-1] = 1;
+	cnt[n - 1][m - 1] = 1;
 
-    dfs(0, 0);
+	dfs(0, 0);
 
-    printf("%d\n", cnt[0][0]);
+	printf("%d", cnt[0][0]);
 
-    return 0;
+	return 0;
 }
