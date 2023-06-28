50bac1aa58f47e276a0bf157a22b2f3ddcbb0c66
codenee <119647209+codenee@users.noreply.github.com>
2023-06-28 12:20:23 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int> answer(2, 0);
+    queue<pair<int, int>> q;
+    int n = sequence.size(), len = 1000001, sum = 0;
+    
+    for(int i = 0; i < n; i++){
+        q.push({sequence[i], i});
+        sum += sequence[i];
+        while(sum > k){
+            sum -= q.front().first;
+            q.pop();
+        }
+        if(sum == k && q.size() < len){
+            len = q.size();
+            answer[0] = q.front().second;
+            answer[1] = q.back().second;
+        }
+    }
+    
+    return answer;
+}
