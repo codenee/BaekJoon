a9d7f31b28173a073f9dd07cc00e653d3c87f666
codenee <119647209+codenee@users.noreply.github.com>
2023-09-07 16:57:38 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    map<string, int> hash;
+    for(int i = 0; i < name.size(); i++){
+        hash[name[i]] = yearning[i];
+    }
+    
+    for(int i = 0; i < photo.size(); i++){
+        int sum = 0;
+        for(int j = 0; j < photo[i].size(); j++){
+            sum += hash[photo[i][j]];
+        }
+        answer.push_back(sum);
+    }
+    
+    return answer;
+}
