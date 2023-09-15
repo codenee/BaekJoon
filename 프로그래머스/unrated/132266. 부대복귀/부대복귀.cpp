cd47164b718bcae3e88b945babd905fcdb155960
codenee <119647209+codenee@users.noreply.github.com>
2023-09-15 12:33:13 +0900

    [unrated] Title: 부대복귀, Time: 22.46 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.cpp
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
-    vector<int> answer(sources.size(), 0);
     
     vector<vector<int>> graph(n+1);
     for(const auto& road : roads){
@@ -13,10 +12,11 @@ vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int 
         graph[road[1]].push_back(road[0]);
     }
     
+    vector<int> answer(sources.size(), 0);
     vector<int> dest(n+1, -1);
+    dest[destination] = 0;
     queue<pair<int, int>> Q;
     Q.push({destination, 0});
-    dest[destination] = 0;
     
     while(!Q.empty()){
         int curPos = Q.front().first;
@@ -25,7 +25,7 @@ vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int 
         for(int i = 0; i < graph[curPos].size(); i++){
             int nextPos = graph[curPos][i];
             if(dest[nextPos] == -1 || dest[nextPos] > curCnt + 1){
-                Q.push({nextPos, curCnt+1});
+                Q.push({nextPos, curCnt + 1});
                 dest[nextPos] = curCnt + 1;
             }
         }
@@ -34,5 +34,6 @@ vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int 
     for(int i = 0; i < sources.size(); i++){
         answer[i] = dest[sources[i]];
     }
+    
     return answer;
 }
