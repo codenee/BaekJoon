c82ac450f707fb0d0c4f2a7d8c1c8970e38768ed
seo <119647209+codenee@users.noreply.github.com>
2024-04-06 11:34:23 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.13 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 0.04 ms
+메모리: 74.6 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 10:58:37
+2024년 04월 06일 11:34:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,5 +1,17 @@
 class Solution {
-    public int[] solution(long n) {
+    
+    public int[] solution2(long n){
+        String str = Long.toString(n);
+        int nLength = str.length();
+
+        int[] answer = new int[nLength];
+        for (int i = 0; i < nLength; i++) {
+            answer[i] = ( str.charAt(nLength - i - 1) - '0');
+        }
+        return answer;
+    }
+    
+    public int[] solution3(long n) {
         
       String str = Long.toString(n);
         String reversed = new StringBuilder(str).reverse().toString();
@@ -9,9 +21,16 @@ class Solution {
         for(int i = 0; i < answer.length; i++){
             answer[i] = arr[i] - '0';
         }
-        
-        
-        
+        return answer;
+    }
+    public int[] solution(long n){
+        String str = Long.toString(n);
+        String reversed = new StringBuilder(str).reverse().toString();
+        int[] answer = new int[reversed.length()];
+        int index = 0;
+        for(char c : reversed.toCharArray()){
+            answer[index++] = c - '0';
+        }
         return answer;
     }
 }
